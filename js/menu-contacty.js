d6ef1f2133767951b79d5d0c230d18f72196d026
priz80 <priz8090@gmail.com>
2025-09-16 14:08:21 +0300

    Create menu-contacty.js

---
+++ js/menu-contacty.js
@@ -0,0 +1,84 @@
+const ballslider = document.getElementById("ballslider");
+
+ballslider.addEventListener("mouseover", () => {
+    ballslider.classList.remove("ballslider");
+    ballslider.classList.add("ballsliders");
+    circleslider.classList.remove("circleslider");
+    circleslider.classList.add("circlesliders");
+
+  });
+  ballslider.addEventListener("mouseout", () => {
+    ballslider.classList.remove("ballsliders");
+    ballslider.classList.add("ballslider");
+    circleslider.classList.remove("circlesliders");
+    circleslider.classList.add("circleslider");
+  });
+
+
+const ballsliderone = document.getElementById("ballsliderone");
+
+ballsliderone.addEventListener("mouseover", () => {
+    ballsliderone.classList.remove("ballslider");
+    ballsliderone.classList.add("ballsliders");
+    circlesliderone.classList.remove("circleslider");
+    circlesliderone.classList.add("circlesliders");
+
+  });
+  ballsliderone.addEventListener("mouseout", () => {
+    ballsliderone.classList.remove("ballsliders");
+    ballsliderone.classList.add("ballslider");
+    circlesliderone.classList.remove("circlesliders");
+    circlesliderone.classList.add("circleslider");
+  });
+
+  const ballslidertoo = document.getElementById("ballslidertoo");
+
+ballslidertoo.addEventListener("mouseover", () => {
+    ballslidertoo.classList.remove("ballslider");
+    ballslidertoo.classList.add("ballsliders");
+    circleslidertoo.classList.remove("circleslider");
+    circleslidertoo.classList.add("circlesliders");
+    telegram.classList.remove("telegram");
+    telegram.classList.add("telegrams");
+  });
+  ballslidertoo.addEventListener("mouseout", () => {
+    ballslidertoo.classList.remove("ballsliders");
+    ballslidertoo.classList.add("ballslider");
+    circleslidertoo.classList.remove("circlesliders");
+    circleslidertoo.classList.add("circleslider");
+    telegram.classList.remove("telegrams");
+    telegram.classList.add("telegram");
+  });
+
+  const ballsliderthree = document.getElementById("ballsliderthree");
+
+ballsliderthree.addEventListener("mouseover", () => {
+    ballsliderthree.classList.remove("ballslider");
+    ballsliderthree.classList.add("ballsliders");
+    circlesliderthree.classList.remove("circleslider");
+    circlesliderthree.classList.add("circlesliders");
+    
+
+  });
+  ballsliderthree.addEventListener("mouseout", () => {
+    ballsliderthree.classList.remove("ballsliders");
+    ballsliderthree.classList.add("ballslider");
+    circlesliderthree.classList.remove("circlesliders");
+    circlesliderthree.classList.add("circleslider");
+  });
+
+  const ballsliderfour = document.getElementById("ballsliderfour");
+
+ballsliderfour.addEventListener("mouseover", () => {
+    ballsliderfour.classList.remove("ballslider");
+    ballsliderfour.classList.add("ballsliders");
+    circlesliderfour.classList.remove("circleslider");
+    circlesliderfour.classList.add("circlesliders");
+
+  });
+  ballsliderfour.addEventListener("mouseout", () => {
+    ballsliderfour.classList.remove("ballsliders");
+    ballsliderfour.classList.add("ballslider");
+    circlesliderfour.classList.remove("circlesliders");
+    circlesliderfour.classList.add("circleslider");
+  });
